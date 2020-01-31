a2c0b6af41556cbaa5f9f200cc971c7559ffb679
Jef De Smedt <Jef.DeSmedt@betavzw.org>
2020-01-31 07:20:55 +0100

    eerste

---
+++ create_admin.ps1
@@ -0,0 +1,7 @@
+﻿param(
+    [string]$pwd,
+    [string]$username = 'administrator'
+)
+$securepawd = ConvertTo-SecureString $pwd -AsPlainText -Force
+New-LocalUser "administrator" -Password $securepawd -Description "Beheerder"
+Add-LocalGroupMember -Group "Administrators" -Member "administrator"
