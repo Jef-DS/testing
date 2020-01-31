d0362acc0f5fa6a155c6dacf724acd3dbee3a8e5
Jef De Smedt <Jef.DeSmedt@betavzw.org>
2020-01-31 10:24:11 +0100

    vierde

--- create_admin.ps1
+++ create_admin.ps1
@@ -1,7 +1,14 @@
-﻿param(
+﻿<#param(
     [string]$pwd,
     [string]$username = 'administrator'
 )
 $securepawd = ConvertTo-SecureString $pwd -AsPlainText -Force
 New-LocalUser "administrator" -Password $securepawd -Description "Beheerder"
-Add-LocalGroupMember -Group "Administrators" -Member "administrator"
+Add-LocalGroupMember -Group "Administrators" -Member "administrator"#>
+(Get-WmiObject -class "Win32_TSGeneralSetting" -Namespace root\cimv2\terminalservices -ComputerName "localhost" -Filter "TerminalName='RDP-tcp'").SetUserAuthenticationRequired(0)
+$lijst=Get-WinUserLanguageList
+$lijst.insert(0, 'nl-BE')
+Set-WinUserLanguageList $lijst -Force
+New-PSDrive -Name HKU -PSProvider Registry -Scope Global -Root HKEY_USERS
+Set-ItemProperty -Path "HKU:\.Default\Keyboard Layout\preload" -Name 1 -Value "00000409"
+Set-ItemProperty -Path "HKU:\.Default\Keyboard Layout\preload" -Name 2 -Value "00000813"
